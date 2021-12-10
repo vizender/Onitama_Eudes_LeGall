7af5b8fed8483b7325d9522f75c38376ba873684
Le Gall Victor <victor@looping.org>
2021-12-10 17:48:11 +0100

    Images pions cases ajoutées

--- Onitama_Eudes_LeGall_graph/src/Pion.java
+++ Onitama_Eudes_LeGall_graph/src/Pion.java
@@ -2,6 +2,9 @@
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -12,9 +15,17 @@ import javax.swing.JButton;
  * @author victorlegall
  */
 public class Pion extends JButton {
+    
+    ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getResource("/images/Case_Vide.png"));
+    ImageIcon img_pion_rouge = new javax.swing.ImageIcon(getClass().getResource("/images/Pion_Rouge.png"));
+    ImageIcon img_pion_bleu = new javax.swing.ImageIcon(getClass().getResource("/images/Pion_Bleu.png"));
+    ImageIcon img_roi_rouge = new javax.swing.ImageIcon(getClass().getResource("/images/Roi_Rouge.png"));
+    ImageIcon img_roi_bleu = new javax.swing.ImageIcon(getClass().getResource("/images/Roi_Bleu.png"));
+    ImageIcon img_castle_rouge = new javax.swing.ImageIcon(getClass().getResource("/images/Case_Castle_Down.png"));
+    ImageIcon img_castle_bleu = new javax.swing.ImageIcon(getClass().getResource("/images/Case_Castle_Up.png"));
+    
     String couleur;
     String type;
-    ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getResource(""));
     
     public Pion(String color){
         couleur = color;
@@ -28,5 +39,12 @@ public class Pion extends JButton {
             return "noir";
         }
         return "erreur";
+    }
+    
+    @Override
+    public void paintComponent ( Graphics G) {
+        super.paintComponent(G);
+        setIcon(img_vide);
+        
     }
 }
